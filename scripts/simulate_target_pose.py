ca415ec27f6ddde9e11038d694f5955878b9f5cc
Riccardo Burlizzi <riccardo.burlizzi@kuleuven.be>
2024-09-20 11:22:10 +0200

    Adapted to have online adaptation to new tgt; added des_ee marker

--- scripts/simulate_target_pose.py
+++ scripts/simulate_target_pose.py
@@ -20,9 +20,14 @@ class SimulateTargetPose:
 
             # Set the position of the target pose, following a circular motion
             angle = 2 * np.pi * self.frequency * (current_time - start_time)
-            self.pose.position.x = 0.4788147543322843758 #0.9056+self.radius * np.cos(angle)
-            self.pose.position.y = -0.1021277091361127431 #0.0635+self.radius * np.sin(angle)
-            self.pose.position.z = 0.2434438793779414023 #0.441+0.1
+            if current_time - start_time > 12: #13
+                self.pose.position.x = 0.5288147543322843758 #0.9056+self.radius * np.cos(angle)
+                self.pose.position.y = -0.35021277091361127431 #0.0635+self.radius * np.sin(angle)
+                self.pose.position.z = 0.2434438793779414023 #0.441+0.1
+            else:
+                self.pose.position.x = 0.4788147543322843758 
+                self.pose.position.y = -0.1021277091361127431 
+                self.pose.position.z = 0.2434438793779414023 
 
             # Keep the orientation constant
             # quaternion = quaternion_from_euler(0, 0, 0)    
--- scripts/trajectory_generation_robot.py
+++ scripts/trajectory_generation_robot.py
@@ -33,11 +33,14 @@ class ROSInvariantTrajectoryGeneration:
         
         # Create a ROS topic subscribers and publishers
         rospy.Subscriber('/target_pose_pub', Pose, self.callback_target_pose)
-        rospy.Subscriber('/start_traj_pub', Pose, self.callback_start_pose)
+        rospy.Subscriber('/current_pose_pub', Pose, self.callback_start_pose)
+        rospy.Subscriber('/progress_partial', std_msgs.msg.Float64, self.callback_progress)
+        rospy.Subscriber('/des_ee', std_msgs.msg.Float64MultiArray, self.callback_des_ee)
         self.publisher_traj_gen_marker = rospy.Publisher('/trajectory_marker', Marker, queue_size=10)
         self.publisher_traj_meas = rospy.Publisher('/target_pose_marker', Marker, queue_size=10)
         self.publisher_joint_values = rospy.Publisher('/joint_states', JointState, queue_size=10)
         self.publisher_trajectory = rospy.Publisher('/trajectory_pub', Trajectory, queue_size=10)
+        self.publisher_desee_marker = rospy.Publisher('/des_ee_marker', Marker, queue_size=10)
 
         # Initialize invariant trajectory generation problem
         self.invariant_model = rw.read_invariants_from_csv(invariant_model_location)
@@ -54,6 +57,11 @@ class ROSInvariantTrajectoryGeneration:
         loaded_FSr = np.loadtxt(FSr_location, delimiter=",")
         self.FSr = loaded_FSr.reshape(loaded_FSr.shape[0], loaded_FSr.shape[1] // 3, 3)
 
+        self.previous_target = [1000,1000,1000]
+
+        self.des_ee = [0,0,0]
+        self.counter = 0
+
     def callback_target_pose(self, pose_msg):
         # Callback function to process the received Pose message
         self.p_end = [pose_msg.position.x, pose_msg.position.y, pose_msg.position.z]
@@ -68,9 +76,9 @@ class ROSInvariantTrajectoryGeneration:
         marker.type = marker.SPHERE
         marker.action = marker.ADD
         # Set marker properties
-        marker.scale.x = 0.05
-        marker.scale.y = 0.05
-        marker.scale.z = 0.05
+        marker.scale.x = 0.02
+        marker.scale.y = 0.02
+        marker.scale.z = 0.02
         marker.color.a = 1.0
         marker.color.r = 0.0
         marker.color.g = 0.0
@@ -88,23 +96,27 @@ class ROSInvariantTrajectoryGeneration:
         quat_obj_start = np.array([start_pose.orientation.x, start_pose.orientation.y, start_pose.orientation.z, start_pose.orientation.w])
         self.Robj_start = R.from_quat(quat_obj_start).as_matrix()
 
+    def callback_progress(self, progress_msg):
+        # Callback function to process the received Float64 message
+        self.current_progress = progress_msg.data
+
+    def callback_des_ee(self, des_ee_msg):
+        # Callback function to process the received Float64MultiArray message
+        self.des_ee = des_ee_msg.data
+
     def run(self):
         if self.p_end is None or self.Robj_end is None:
             print("Waiting for a target pose to be published on /target_pose_pub topic...")
             rospy.wait_for_message('/target_pose_pub', Pose)
         if self.p_start is None or self.Robj_start is None:
-            print("Waiting for a start pose to be published on /start_traj_pub topic...")
-            rospy.wait_for_message('/start_traj_pub', Pose)
+            print("Waiting for a start pose to be published on /current_pose_pub topic...")
+            rospy.wait_for_message('/current_pose_pub', Pose)
         # self.p_start = np.array([-2.622134222757627892e-02, -4.767094578195006371e-01, 0.08945761571123636569])
         # self.Robj_start = R.from_quat(np.array([-7.111921398743396017e-01,-8.563669717168140294e-03,-7.027414404159517680e-01,-1.693728620663667583e-02])).as_matrix()
         if self.Robj_start is not None and self.Robj_end is not None and self.p_start is not None and self.p_end is not None:
-            print(self.p_start)
-            print(self.Robj_start)
-            current_progress = 0
+            
+            # current_progress = 0.6 # TAKE THIS FROM ETASLCORE
             number_samples = 100
-            arclength_n = np.linspace(0,1,number_samples)
-
-            progress_values = np.linspace(current_progress, arclength_n[-1], number_samples)
 
             # new constraints       
             alpha = 0
@@ -150,10 +162,12 @@ class ROSInvariantTrajectoryGeneration:
             # Franka q_init: [-0.03594372660758203, -1.7589981400814376, -1.9254516473826875, -1.9436872720551073, -0.3177960551049983, 1.981110099809029,-1.0311961190588514]
             
             FS_online_generation_problem = OCP_gen.OCP_gen_pose(boundary_constraints,number_samples,fatrop_solver=True,robot_params=robot_params)
-
+            
             # Resample model invariants to desired number of number_samples samples
+            current_sample = round(self.current_progress*len(self.invariant_model))
+            arclength_n = np.linspace(0,1,number_samples)
             spline_invariant_model = sh.create_spline_model(self.invariant_model[:,0], self.invariant_model[:,1:])
-            progress_values = np.linspace(self.invariant_model[0,0],self.invariant_model[-1,0],number_samples)
+            progress_values = np.linspace(self.current_progress,self.invariant_model[-1,0],number_samples)
             model_invariants,progress_step = sh.interpolate_invariants(spline_invariant_model, progress_values)
 
             # Define OCP weights
@@ -192,66 +206,102 @@ class ROSInvariantTrajectoryGeneration:
 
             while not rospy.is_shutdown():
 
-                # Specify the boundary constraints
-                boundary_constraints["position"]["initial"] = self.p_start # self.demo_pos[0] # np.array([0.3056, 0.0635, 0.441]) #
-                boundary_constraints["position"]["final"] = self.p_end # self.demo_pos[-1] #self.p_obj[-1] # np.array([0.827,0.144,0.552]) #np.array([0.69,0.244,0.4]) #
-                
+                # Generate a new trajectory only if the target has changed
+                if np.linalg.norm(np.array(self.p_end) - np.array(self.previous_target)) > 1E-6:
 
-                # Generate trajectory
-                invariants, pos, R_obj, R_t, R_r, time, joint_values = FS_online_generation_problem.generate_trajectory(model_invariants,boundary_constraints,progress_step,weights_params,initial_values)
+                    self.previous_target = self.p_end
 
-                # fig = plt.figure(figsize=(14,8))
-                # ax = fig.add_subplot(111, projection='3d')
-                # ax.plot(self.demo_pos[:,0],self.demo_pos[:,1],self.demo_pos[:,2],'b')
-                # ax.plot(pos[:,0],pos[:,1],pos[:,2],'r')
-                # pl.plot_invariants(model_invariants, invariants, arclength_n, progress_values)
-                # indx_online = np.trunc(np.linspace(0,len(pos)-1,5))
-                # indx_online = indx_online.astype(int)
-                # for i in indx_online:
-                #     pl.plot_3d_frame(self.demo_pos[i,:],self.demo_Robj[i,:,:],1,0.01,['red','green','blue'],ax)
-                #     pl.plot_3d_frame(pos[i,:],R_obj[i,:,:],1,0.01,['red','green','blue'],ax)
-                # plt.show()
+                    # Specify the boundary constraints
+                    boundary_constraints["position"]["initial"] = self.p_start # self.demo_pos[0] # np.array([0.3056, 0.0635, 0.441]) #
+                    boundary_constraints["position"]["final"] = self.p_end # self.demo_pos[-1] #self.p_obj[-1] # np.array([0.827,0.144,0.552]) #np.array([0.69,0.244,0.4]) #
+                    boundary_constraints["orientation"]["initial"] = self.demo_Robj[current_sample]
+                    boundary_constraints["moving-frame"]["translational"]["initial"] = orthonormalize(self.FSt[current_sample])
+                    boundary_constraints["moving-frame"]["rotational"]["initial"] = orthonormalize(self.FSr[current_sample])
 
-                # Publish trajectory
-                trajectory = Trajectory()
-                quaternion = rot2quat(R_obj)
-                for i in range(invariants.shape[0]):
-                    pose = Pose()
-                    pose.position.x = pos[i,0]
-                    pose.position.y = pos[i,1]
-                    pose.position.z = pos[i,2]
-                    pose.orientation.x = quaternion[i,0]
-                    pose.orientation.y = quaternion[i,1]
-                    pose.orientation.z = quaternion[i,2]
-                    pose.orientation.w = quaternion[i,3]
-                    trajectory.poses.append(pose)
-                self.publisher_trajectory.publish(trajectory)
+                    # Generate trajectory
+                    invariants, pos, R_obj, R_t, R_r, time, joint_values = FS_online_generation_problem.generate_trajectory(model_invariants,boundary_constraints,progress_step,weights_params,initial_values)
 
-                # Add points to the marker
-                marker.points = []
-                for point in pos:  # Assuming traj is a numpy array of shape (number_samples, 3)
-                    p = Point()
-                    p.x = point[0]
-                    p.y = point[1]
-                    p.z = point[2]
-                    marker.points.append(p)
+                    # fig = plt.figure(figsize=(14,8))
+                    # ax = fig.add_subplot(111, projection='3d')
+                    # ax.plot(self.demo_pos[:,0],self.demo_pos[:,1],self.demo_pos[:,2],'b')
+                    # ax.plot(pos[:,0],pos[:,1],pos[:,2],'r')
+                    # pl.plot_invariants(model_invariants, invariants, arclength_n, progress_values)
+                    # indx_online = np.trunc(np.linspace(0,len(pos)-1,5))
+                    # indx_online = indx_online.astype(int)
+                    # for i in indx_online:
+                    #     pl.plot_3d_frame(self.demo_pos[i,:],self.demo_Robj[i,:,:],1,0.01,['red','green','blue'],ax)
+                    #     pl.plot_3d_frame(pos[i,:],R_obj[i,:,:],1,0.01,['red','green','blue'],ax)
+                    # plt.show()
 
-                # Publish the Marker
-                marker.header.stamp = rospy.Time.now() # add timestamp
-                self.publisher_traj_gen_marker.publish(marker)
-
-                # Move robot
-                if robot_params["urdf_file_name"] is not None:
-                    path_to_urdf = rw.find_robot_path(robot_params["urdf_file_name"])
-                    robot = urdf.URDF.load(path_to_urdf)
-                    joints = JointState()
-                    joints.name = [robot._actuated_joints[i].name for i in range(robot.num_actuated_joints)]
+                    # Publish trajectory
+                    trajectory = Trajectory()
+                    quaternion = rot2quat(R_obj)
                     for i in range(invariants.shape[0]):
-                        joints.position = joint_values[i]
-                        joints.header.stamp = rospy.Time.now()
-                        # print(joint_values[i])
-                        self.publisher_joint_values.publish(joints)
-                        rospy.sleep(0.1)
+                        pose = Pose()
+                        pose.position.x = pos[i,0]
+                        pose.position.y = pos[i,1]
+                        pose.position.z = pos[i,2]
+                        pose.orientation.x = quaternion[i,0]
+                        pose.orientation.y = quaternion[i,1]
+                        pose.orientation.z = quaternion[i,2]
+                        pose.orientation.w = quaternion[i,3]
+                        trajectory.poses.append(pose)
+                    self.publisher_trajectory.publish(trajectory)
+
+                    # Add points to the marker
+                    marker.points = []
+                    for point in pos:  # Assuming traj is a numpy array of shape (number_samples, 3)
+                        p = Point()
+                        p.x = point[0]
+                        p.y = point[1]
+                        p.z = point[2]
+                        marker.points.append(p)
+
+                    # Publish the Marker
+                    marker.header.stamp = rospy.Time.now() # add timestamp
+                    self.publisher_traj_gen_marker.publish(marker)
+
+                    # Move robot
+                    if robot_params["urdf_file_name"] is not None:
+                        path_to_urdf = rw.find_robot_path(robot_params["urdf_file_name"])
+                        robot = urdf.URDF.load(path_to_urdf)
+                        joints = JointState()
+                        joints.name = [robot._actuated_joints[i].name for i in range(robot.num_actuated_joints)]
+                        for i in range(invariants.shape[0]):
+                            joints.position = joint_values[i]
+                            joints.header.stamp = rospy.Time.now()
+                            # print(joint_values[i])
+                            self.publisher_joint_values.publish(joints)
+                            rospy.sleep(0.1)
+
+                marker_msg = Marker()
+                marker_msg.header.frame_id = "world"
+                marker_msg.header.stamp = rospy.Time.now()
+                marker_msg.type = marker_msg.CUBE
+                # marker_msg.action = marker_msg.ADD
+                marker_msg.lifetime = rospy.Duration(30)
+                marker_msg.id = self.counter
+                self.counter += 1
+                # marker scale
+                marker_msg.scale.x = 0.008
+                marker_msg.scale.y = 0.008
+                marker_msg.scale.z = 0.008
+                # marker color
+                marker_msg.color.a = 1.0
+                marker_msg.color.r = 0.0
+                marker_msg.color.g = 0.0
+                marker_msg.color.b = 1.0
+                # marker orientaiton
+                marker_msg.pose.orientation.x = 0.0
+                marker_msg.pose.orientation.y = 0.0
+                marker_msg.pose.orientation.z = 0.0
+                marker_msg.pose.orientation.w = 1.0
+                # add all the points to the marker message
+                marker_msg.pose.position.x = self.des_ee[0]
+                marker_msg.pose.position.y = self.des_ee[1]
+                marker_msg.pose.position.z = self.des_ee[2]
+                # add the marker to the publisher
+                self.publisher_desee_marker.publish(marker_msg)
 
                 self.update_rate.sleep() # Sleep to maintain the specified update rate
 
