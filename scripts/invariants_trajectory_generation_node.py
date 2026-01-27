f5171715482ef5ee0133c72300a6e83e142b273c
Riccardo Burlizzi <riccardo.burlizzi@kuleuven.be>
2026-01-27 10:14:09 +0200

    Continue editing of new invariants traj gen node

--- scripts/invariants_trajectory_generation_node.py
+++ scripts/invariants_trajectory_generation_node.py
@@ -92,18 +92,16 @@ class invariants_traj_gen_node:
         rospy.Subscriber('sim_bottle_pos_pub', Float64MultiArray, self.callback_bottle_pos_sim)
         rospy.Subscriber('/pickit/objects_wrt_robot_frame', Float64MultiArray, self.callback_bottle_pos_real) # THIS IS EXAMPLE OF BOTTLE POSITION DETECTED BY SENSOR - NOT IMPLEMENTED CORRECTLY YET (update also in listener_node)
         rospy.Subscriber('pose_w_tcp', Float64MultiArray, self.callback_tf)
-        rospy.Subscriber('progress_partial', Float64MultiArray, self.callback_progress)
-        # rospy.Subscriber('progress', Float64MultiArray, self.callback_progress)
-        rospy.Subscriber('progress_fv', Float64MultiArray, self.callback_progress_fv)
-        rospy.Subscriber('progress_offset_previous', Float64MultiArray, self.callback_progress_offset_previous)
+        rospy.Subscriber('progress', Float64MultiArray, self.callback_progress) # I need some kind of progress, representing the progress of the robot along the trajectory
+        rospy.Subscriber('progress_fv', Float64MultiArray, self.callback_progress_fv) # This should be the value of the progress feature variable
+        rospy.Subscriber('progress_offset_previous', Float64MultiArray, self.callback_progress_offset_previous) # It's the progress offset of the previous sample
         rospy.Subscriber('condition_output', Float64MultiArray, self.callback_cond)
-        rospy.Subscriber('tgt_frame_wrt_tgt', Float64MultiArray, self.callback_inst_tgt)
-        rospy.Subscriber('jointvel', Float64MultiArray, self.callback_jointvel)
         rospy.Subscriber('factor_execution_speed', Float64MultiArray, self.callback_factorexecspeed)
         self.pub_node_output = rospy.Publisher('inv_gen_node_output', Float64MultiArray, queue_size=10, latch=True)
         self.publisher_trajectory = rospy.Publisher('/trajectory_pub', Trajectory, queue_size=10)
+        self.publisher_traj_gen_marker = rospy.Publisher('/trajectory_marker', Marker, queue_size=10)
 
-        if self.pos_w_tgt is None or self.R_w_tgt is None:
+        if self.pos_w_tgt is None:
             print("Waiting for a target pose to be published on /target_pose_pub topic...")
             rospy.wait_for_message('/target_pose_pub', Pose)
         if self.tf is None:
@@ -112,12 +110,17 @@ class invariants_traj_gen_node:
             rospy.wait_for_message('/progress_partial',std_msgs.msg.Float64)
            
         self.number_samples = 100
+
+        # new constraints       
+        alpha = 0
+        rotate = R.from_euler('z', alpha, degrees=True)
+        R_w_tgt =  orthonormalize(rotate.as_matrix() @ self.demo_Robj[-1])
+        FSt_end = orthonormalize(rotate.as_matrix() @ self.demo_FSt[-1])
         
         # # Linear initialization
-        R_obj_init = interpR(np.linspace(0, 1, len(self.demo_pos)), [0,1], np.array([self.R_w_tcp, self.R_w_tgt]))
-        # # R_r_init = interpR(np.linspace(0, 1, len(optim_calc_results.FSr_frames)), [0,1], np.array([FSr_start, FSr_end]))
+        R_obj_init = interpR(np.linspace(0, 1, len(self.demo_pos)), [0,1], np.array([np.eye(3), R_w_tgt]))
 
-        R_r_init, R_r_init_array, invars_init = FSr_init(self.R_w_tcp, self.R_w_tgt)
+        R_r_init, R_r_init_array, invars_init = FSr_init(np.eye(3), R_w_tgt)
         
         # Initialising the boundary constraints with zeros or I
         self.boundary_constraints = {
@@ -217,10 +220,6 @@ class invariants_traj_gen_node:
 
         self.cond = data.data[0]
 
-    def callback_inst_tgt(self,data):
-
-        self.inst_tgt = data.data
-
     def callback_jointvel(self,data):
 
         self.jointvel = data.data
@@ -240,7 +239,7 @@ class invariants_traj_gen_node:
                 self.pos_w_bottle = self.bottle_pos_real
         
             if self.enter_recovery_mode == 0:
-                self.previous_model_trajectory = self.model_trajectory[:,:3]
+                self.previous_model_trajectory = self.pos_w_traj
 
             # Define the target pose, by adding the bottle radius distance to the bottle position measurement and a small offset to ensure no contact between opener and bottle
             # Rotate the target pose around the z-axis by the alpha_deg angle (currently fixed), defining the desired approach direction
@@ -254,7 +253,7 @@ class invariants_traj_gen_node:
             self.pose_w_tgt = np.hstack([self.pos_w_tgt,np.array([quat_w_tgt[3],quat_w_tgt[0],quat_w_tgt[1],quat_w_tgt[2]])])
 
             # Check if the robot position is within 0.01 m of the model trajectory
-            dist_robot_to_model = np.array([np.linalg.norm(self.tf[:3] - self.pos_w_model[i,:]) for i in range(self.number_samples)])
+            dist_robot_to_model = np.array([np.linalg.norm(self.tf[:3] - self.pos_w_traj[i,:]) for i in range(self.number_samples)])
             if any(dist_robot_to_model  < 0.01): # it checks if the robot is within 1cm away of the interp_samples of the model trajectory
                 self.condition = 1
                 self.current_sample = np.argmin(dist_robot_to_model)
@@ -264,22 +263,20 @@ class invariants_traj_gen_node:
     def generate_trajectory(self):
         if not self.tf[0] == 0:# and self.condition == 0:
 
-            # Take the current robot pose (coming from TF topic) and convert the quaternion orientation into a rotation matrix
-            # pos_w_tcp = np.array([self.tf[0],self.tf[1],self.tf[2]])
-            # R_w_tcp = quat2rot(np.hstack([self.tf[3], self.tf[4], self.tf[5],self.tf[6]]).reshape(1,4)).reshape(3,3)
+            # Predict the robot pose in 100ms (ros node rate) by taking the model pose in a few delay_sample
             delay_sample = 2+int(self.factor_execution_speed/2)# int(2*self.factor_execution_speed) # (-self.enter_recovery_mode+1) # (-2*self.enter_recovery_mode+1) * 
             if all(self.jointvel[i] < 1e-4 for i in range(6)) or self.enter_recovery_mode == 1:
                 pos_w_tcp = np.array([self.tf[0],self.tf[1],self.tf[2]])
             else:
                 if self.current_sample+ delay_sample>= len(self.previous_model_trajectory):
-                    pos_w_tcp = self.previous_model_trajectory[self.current_sample] #np.array([self.inst_tgt[0],self.tf[1],self.inst_tgt[2]])
+                    pos_w_tcp = self.previous_model_trajectory[self.current_sample]
                 else:
-                    pos_w_tcp = self.previous_model_trajectory[self.current_sample+delay_sample] #np.array([self.inst_tgt[0],self.tf[1],self.inst_tgt[2]])
+                    pos_w_tcp = self.previous_model_trajectory[self.current_sample+delay_sample]
             R_w_tcp = quat2rot(np.hstack([self.tf[3], self.tf[4], self.tf[5],self.tf[6]]).reshape(1,4)).reshape(3,3)
 
             if self.counter == 0:
                 self.previous_target = self.pos_w_tgt
-            prev_dist_to_target = np.linalg.norm(self.previous_target)
+            prev_dist_to_target = np.linalg.norm(self.previous_target - pos_w_tcp)
             if self.condition == 0:
                 # Calculate s_prior by comparing the distance of the current robot pose to the previous target and to the current target
                 dist_to_target = np.linalg.norm(self.pos_w_tgt - pos_w_tcp)
@@ -307,23 +304,23 @@ class invariants_traj_gen_node:
                 self.boundary_constraints["position"]["initial"] = pos_w_tcp
                 self.boundary_constraints["position"]["final"] = self.pos_w_tgt
                 self.boundary_constraints["orientation"]["initial"] = R_w_tcp
-                self.boundary_constraints["orientation"]["final"] = self.R_w_tgt
+                self.boundary_constraints["orientation"]["final"] = R_w_tgt # Start simple, where R_w_tgt is constant and equal to demo_Robj (for cf), TODO add variability in orientation!
                 self.boundary_constraints["moving-frame"]["translational"]["initial"] = orthonormalize(self.FSt_w_traj[current_sample])
                 self.boundary_constraints["moving-frame"]["translational"]["final"] = FSt_end
                 self.boundary_constraints["moving-frame"]["rotational"]["initial"] = orthonormalize(self.FSr_w_traj[current_sample])
                 self.boundary_constraints["moving-frame"]["rotational"]["initial"] = self.demo_FSr
 
                 # Generate trajectory
-                invariants, pos, R_obj, self.FSt_w_traj, self.FSr_w_traj, joint_values = self.FS_online_generation_problem.generate_trajectory(model_invariants,self.boundary_constraints,progress_step,self.weights_params,self.initial_values)
+                self.invariants_traj, self.pos_w_traj, self.R_w_traj, self.FSt_w_traj, self.FSr_w_traj, joint_values = self.FS_online_generation_problem.generate_trajectory(model_invariants,self.boundary_constraints,progress_step,self.weights_params,self.initial_values)
 
             # Publish trajectory
             trajectory = Trajectory()
-            quaternion = rot2quat(R_obj)
-            for i in range(invariants.shape[0]):
+            quaternion = rot2quat(self.R_w_traj)
+            for i in range(self.invariants_traj.shape[0]):
                 pose = Pose()
-                pose.position.x = pos[i,0]
-                pose.position.y = pos[i,1]
-                pose.position.z = pos[i,2]
+                pose.position.x = self.pos_w_traj[i,0]
+                pose.position.y = self.pos_w_traj[i,1]
+                pose.position.z = self.pos_w_traj[i,2]
                 pose.orientation.x = quaternion[i,0]
                 pose.orientation.y = quaternion[i,1]
                 pose.orientation.z = quaternion[i,2]
@@ -333,7 +330,7 @@ class invariants_traj_gen_node:
 
             # Add points to the marker
             self.marker.points = []
-            for point in pos:  # Assuming traj is a numpy array of shape (self.number_samples, 3)
+            for point in self.pos_w_traj:  # Assuming traj is a numpy array of shape (self.number_samples, 3)
                 p = Point()
                 p.x = point[0]
                 p.y = point[1]
@@ -346,47 +343,23 @@ class invariants_traj_gen_node:
 
             self.counter += 1
 
-                # if dist_err > 0.0065:
-                #     self.enter_recovery_mode = 1
-                #     print("In recovery mode")
-                # else:
-                #     self.enter_recovery_mode = 0
-                #     self.published_latent_variables = self.latent_variables.copy()
-                #     # Calculate the new progress offset that results into the casadi_progress
-                #     self.progress_offset = self.progress_offset_previous + s_prior - self.progress_sum
-
-                # print(self.alpha,self.counter)
-
-            if self.enter_recovery_mode == 0:
-                self.recovery_target = self.pose_w_tgt.copy()
-            else:
-                self.pose_w_tgt = self.recovery_target.copy()
-
             self.previous_target = self.pos_w_tgt
 
-    # def publish(self):
-    #     # This function publishes all the output topics of the node.
-    #     # It was created to split publishing from calculations and reduce the risk of having etasl running in the middle of a node loop.
-    #     if not self.tf[0] == 0:
-    #         self.pub_ext_condition.publish(self.condition)
+            # TODO Implement recovery strategy
 
-    #         # If the condition is 0 (meaning that the robot pose is not on the model trajectory), publish the latent variables and the progress offset.
-    #         outputs = np.hstack((self.published_latent_variables, self.progress_offset, self.pose_w_tgt, self.dist_ratio, self.enter_recovery_mode,self.alpha))
-
-    #         self.pub_modeltraj_markers.publish(self.marker_msg)
+            self.progress_offset = self.progress_offset_previous + s_prior - self.progress_sum
             
-    #         self.pub_node_output.publish(Float64MultiArray(data=outputs))
+            outputs = np.hstack((self.progress_offset, self.enter_recovery_mode, self.alpha))
+
+            self.pub_node_output.publish(Float64MultiArray(data=outputs))
 
 if __name__ == '__main__':
 
     inv_node = invariants_traj_gen_node("talker")
-
-    inv_node.init_OCP()
 
     # This defines the rate at which the node should publish
     rate = rospy.Rate(10) # 10hz
     while not rospy.is_shutdown():
         inv_node.check_condition()
         inv_node.generate_trajectory()
-        # inv_node.publish()
         rate.sleep()
